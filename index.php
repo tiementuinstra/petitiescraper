cb2522c86936aedafc6f5047623aa4076aaac280
tiementuinstra <mail@tiemenrtuinstra.nl>
2021-06-29 16:19:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,84 @@
+<style>
+div#anchor-scroll{display:none;}
+div.petition-signature-list{
+	display:table-row;
+	width:100%;
+	clear:both;
+}
+div.petition-signature-list div,
+div.petition-signature-list span{
+	display:table-cell;
+	width:25%;
+}
+div.petition-signature-list div.petition-signature-occupation,
+div.petition-signature-list div.petition-signature-actions,
+div.petition-signature-list span.signature-time{
+	display:none;
+}
+</style>
+<?php
+require 'simple_html_dom.php';
+//$url = 'geen-nieuwbouw-in-de-bossen-bij-paleis-soestdijk';
+$url = $_REQUEST['petitie'];
+$url = 'https://petities.nl/petitions/'.$_REQUEST['petitie'].'/signatures?locale=nl';
+$html = file_get_html($url);
+$title = $html->find('h1', 0);
+echo '<a href="'.$url.'"><h1>'.$title->plaintext.'</h1></a>';
+echo '<table border="1">';
+echo '<tr>
+<th>Onderschrijver</th>
+<th>Plaats</th>
+<th>Beroep</th>
+<th>Tijd</th>
+</tr>';
+for($page=0;$page<=50;$page++){
+//	$html = file_get_html($url.'?page='.$i);
+//$petitiecontainer = $html->find('.petition-signatures-container', 0);
+
+//echo $petitiecontainer->xmltext;
+	
+	$signatures = scraping_petitie($url,$page);
+	foreach($signatures as $signature){
+		echo '<tr>';
+		echo '<td>'.$signature['name'].'</td>';
+		echo '<td>'.$signature['location'].'</td>';
+		echo '<td><span>'.$signature['occupation'].'</span></td>';
+		echo '<td>'.$signature['time'].'</td>';
+		echo '</tr>';
+	}
+	
+}
+echo '</table>';
+
+function scraping_petitie($url,$page) {
+    // create HTML DOM
+    $html = file_get_html($url.'?page='.$i);
+
+    // get news block
+    foreach($html->find('div.petition-signature-list') as $signature) {
+        $item['name'] = trim($signature->find('div.petition-signature-name', 0)->plaintext);
+        $item['location'] = trim($signature->find('div.petition-signature-location', 0)->plaintext);
+        $item['occupation'] = trim($signature->find('div.petition-signature-occupation', 0)->plaintext);
+        $item['time'] = trim($signature->find('span.signature-time', 0)->plaintext);
+
+
+        $ret[] = $item;
+    }
+    
+    // clean up memory
+    $html->clear();
+    unset($html);
+
+    return $ret;
+}
+
+
+// -----------------------------------------------------------------------------
+// test it!
+
+// "http://digg.com" will check user_agent header...
+ini_set('user_agent', 'My-Application/2.5');
+
+$ret = scraping_petitie($url);
+
+?>
